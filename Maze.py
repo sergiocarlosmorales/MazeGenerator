a4883f503596602546d916890033cd8dc0836070
sergiocarlos <carloschilazo@gmail.com>
2014-03-30 19:36:26 -0500

    Improve code structure and a few comments here and there

--- Maze.py
+++ Maze.py
@@ -1,25 +1,48 @@
 class ChillMazeTile:
-    _wallTop = True
-    _wallBottom = True
-    _wallRight = True
-    _wallLeft = True
+    # Flags to indicate if a given wall is up
+    _wall_top = None
+    _wall_bottom = None
+    _wall_right = None
+    _wall_left = None
+
+    def __init__(self):
+        """
+        Initializer of a tile
+        """
+        # Build all walls
+        self._set_wall("Top", True)
+        self._set_wall("Right", True)
+        self._set_wall("Bottom", True)
+        self._set_wall("Left", True)
 
     def are_all_walls_up(self):
-        return self._wallBottom and self._wallLeft and \
-            self._wallRight and self._wallTop
+        """Determine whether or not: all walls are up
+            Params:
+                none
+            Returns:
+                bool
+        """
+        return self._wall_bottom and self._wall_left and \
+            self._wall_right and self._wall_top
 
-    def knock_wall(self, location):
+    def _set_wall(self, location, new_value):
         if location.capitalize() == "Top":
-            self._wallTop = False
+            self._wall_top = new_value
         elif location.capitalize() == "Bottom":
-            self._wallBottom = False
+            self._wall_bottom = new_value
         elif location.capitalize() == "Left":
-            self._wallLeft = False
+            self._wall_left = new_value
         elif location.capitalize() == "Top":
-            self._wallTop = False
+            self._wall_top = new_value
         else:
             raise ValueError("Invalid wall location provided:{}"
                              .format(location))
+
+    def knock_wall(self, location):
+        return self._set_wall(location, False)
+
+    def raise_wall(self, location):
+        return self._set_wall(location, True)
 
 
 class ChillMaze:
