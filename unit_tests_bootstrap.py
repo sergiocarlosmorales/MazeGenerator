d895cbfd9849723f4408555f8197406a5c7c1cc7
sergiocarlos <carloschilazo@gmail.com>
2014-04-05 13:29:14 -0500

    Docblocks for unit test bootstrapper

--- unit_tests_bootstrap.py
+++ unit_tests_bootstrap.py
@@ -1,16 +1,25 @@
 import unittest
+# Test classes
 from tile_tests import TileTests
 from maze_tests import MazeTests
+# Implementation classes
 from chill_tile import ChillTile
 from chill_maze import ChillMaze
 
 
-def run_tests_wrapper(test_case):
-    suite = unittest.TestLoader().loadTestsFromTestCase(test_case)
-    unittest.TextTestRunner(verbosity=1).run(suite)
+def run_tests_wrapper(testcase, verbosity_level=1):
+    """This will execute a test case
+        Params:
+            testcase The actual test case, unittest.TestCase
+            verbosity_level Optional numeric to tune the level of output
+    """
+    suite = unittest.TestLoader().loadTestsFromTestCase(testcase)
+    unittest.TextTestRunner(verbosity=verbosity_level).run(suite)
 
 
 def run_tile_tests():
+    """Run tile tests for all tile classes
+    """
     tile_classes = [ChillTile]
     for tile_class in tile_classes:
         TileTests.tile_class = tile_class
@@ -18,6 +27,8 @@ def run_tile_tests():
 
 
 def run_maze_tests():
+    """Run maze tests for all maze classes
+    """
     maze_classes = [ChillMaze]
     for maze_class in maze_classes:
         MazeTests.maze_class = maze_class
